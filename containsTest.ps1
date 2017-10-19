980423063c050478bc43b4a8abb7e3e255165869
jasonsandholm <jason.sandholm@gmail.com>
2017-10-19 10:27:06 -0500

    Added Test script

---
+++ containsTest.ps1
@@ -0,0 +1,29 @@
+﻿#Contains test
+$profiles = get-childitem C:\users\jasonsa\desktop\profiles
+$users = @('jasonsa','troyh','alissah')
+foreach ($profile in $profiles)
+{
+
+     #if folder has .v2 extension this parses it from the username
+    if ($profile.Extension -eq ".v2")
+    {
+        $userName = $profile.name.Split(".")[0]
+    }
+    Else
+    {
+        $userName = $profile.Name
+    }
+
+    if ($users -contains $userName)
+    {
+        Write-Host "Found" + $profile.Name
+        Write-Host $profile.Name 
+        Write-Host $userName
+        
+    }
+    else
+    {
+        Write-Host "Could not find" + $profile.Name
+    }
+}
+
